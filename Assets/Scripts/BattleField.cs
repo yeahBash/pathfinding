7e2f5a1529877b4087e66708929e478f771c72e2
Ilnur_Ishkaev <ilnurishkaev@yandex.ru>
2020-07-11 17:43:03 +0300

    Initialization of the project. Set Grid, Battlefield. Setup UI.

---
+++ Assets/Scripts/BattleField.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Grid))]
+public class BattleField : MonoBehaviour
+{
+    const int BATTLEFIELD_SIZE = 50;
+    private Grid _grid;
+    [SerializeField] GameObject _cellNode;
+    private void Awake()
+    {
+        _grid = GetComponent<Grid>();
+    }
+
+    void Start()
+    {
+        CreateBattleField();
+    }
+
+    private void CreateBattleField()
+    {
+        for (int i = -BATTLEFIELD_SIZE/2; i < BATTLEFIELD_SIZE/2; i++)
+        {
+            for (int j = -BATTLEFIELD_SIZE/2; j < BATTLEFIELD_SIZE/2; j++)
+            {
+                GameObject cellNode = Instantiate(_cellNode, _grid.GetCellCenterLocal(new Vector3Int(i,j,0)), Quaternion.identity);
+                cellNode.transform.SetParent(transform);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Mode
+{
+    Idle,
+    AddStart,
+    AddFinish,
+    AddObstacle,
+    RemoveObstacle
+}
+
+public class Manager : MonoBehaviour
+{
+    public Mode currentMode {get; private set;}
+    public BattleField battleField;
+    public Astar astar;
+
+    public void ChangeMode(int modeInt)
+    {
+        Mode mode = (Mode) modeInt;
+        currentMode = mode;
+    }
+}
